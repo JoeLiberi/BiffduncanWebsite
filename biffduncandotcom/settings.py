43fd5eb027faf7eb98cbf84cbba09e8699014cf6
Joe Liberi <m4punk@gmail.com>
2017-01-23 13:13:08 -0500

    Changed static file dirs

--- biffduncandotcom/settings.py
+++ biffduncandotcom/settings.py
@@ -17,6 +17,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -39,11 +40,13 @@ ALLOWED_HOSTS = []
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(DATA_DIR, 'media')
-STATIC_ROOT = os.path.join(DATA_DIR, 'static')
+# STATIC_ROOT = os.path.join(DATA_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 ROOT_URLCONF = 'biffduncandotcom.urls'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'biffduncandotcom', 'static'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 SITE_ID = 1
 
