1f18802390c5365939d9dc32b2d42576f5ae23e6
Joe Liberi <m4punk@gmail.com>
2017-01-24 23:18:40 -0500

    Set prodcution db to ClearDB MySQL

--- biffduncandotcom/settings.py
+++ biffduncandotcom/settings.py
@@ -1,4 +1,4 @@
-import os, sys, 
+import os, sys
 from urllib.parse import urlparse
 
 gettext = lambda s: s
@@ -178,52 +178,52 @@ CMS_PERMISSION = True
 
 CMS_PLACEHOLDER_CONF = {}
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'CONN_MAX_AGE': 0,
-            'ENGINE': 'django.db.backends.sqlite3',
-            'HOST': 'localhost',
-            'NAME': DATA_DIR+ '/database.db',
-            'PASSWORD': '',
-            'PORT': '',
-            'USER': ''
-        }
-    }
+# if DEBUG:
+#     DATABASES = {
+#         'default': {
+#             'CONN_MAX_AGE': 0,
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'HOST': 'localhost',
+#             'NAME': DATA_DIR+ '/database.db',
+#             'PASSWORD': '',
+#             'PORT': '',
+#             'USER': ''
+#         }
+#     }
 
-else:
-    ''' Heroku Clear MySQLDB '''
-    # Register database schemes in URLs.
-    urlparse.uses_netloc.append('mysql')
+# else:
+#     ''' Heroku Clear MySQLDB '''
+#     # Register database schemes in URLs.
+#     urlparse.uses_netloc.append('mysql')
 
-    try:
+#     try:
 
-        # Check to make sure DATABASES is set in settings.py file.
-        # If not default to {}
+#         # Check to make sure DATABASES is set in settings.py file.
+#         # If not default to {}
 
-        if 'DATABASES' not in locals():
-            DATABASES = {}
+#         if 'DATABASES' not in locals():
+#             DATABASES = {}
 
-        if 'DATABASE_URL' in os.environ:
-            url = urlparse.urlparse(os.environ['DATABASE_URL'])
+#         if 'DATABASE_URL' in os.environ:
+#             url = urlparse.urlparse(os.environ['DATABASE_URL'])
 
-            # Ensure default database exists.
-            DATABASES['default'] = DATABASES.get('default', {})
+#             # Ensure default database exists.
+#             DATABASES['default'] = DATABASES.get('default', {})
 
-            # Update with environment configuration.
-            DATABASES['default'].update({
-                'NAME': url.path[1:],
-                'USER': url.username,
-                'PASSWORD': url.password,
-                'HOST': url.hostname,
-                'PORT': url.port,
-            })
+#             # Update with environment configuration.
+#             DATABASES['default'].update({
+#                 'NAME': url.path[1:],
+#                 'USER': url.username,
+#                 'PASSWORD': url.password,
+#                 'HOST': url.hostname,
+#                 'PORT': url.port,
+#             })
 
 
-            if url.scheme == 'mysql':
-                DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
-    except Exception:
-        print('Unexpected error:', sys.exc_info())
+#             if url.scheme == 'mysql':
+#                 DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
+#     except Exception:
+#         print('Unexpected error:', sys.exc_info())
 
 
 
