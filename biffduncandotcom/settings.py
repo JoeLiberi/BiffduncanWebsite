9d190c0033646e4b78aff59a377419cee6805e47
Joe Liberi <m4punk@gmail.com>
2017-01-24 23:07:53 -0500

    Set prodcution db to ClearDB MySQL

--- biffduncandotcom/settings.py
+++ biffduncandotcom/settings.py
@@ -190,17 +190,40 @@ if DEBUG:
     }
 
 else:
-    import dj_database_url
-    db_from_env = dj_database_url.config(conn_max_age=500)
-    DATABASES['default'].update(db_from_env)
-    # DATABASES = {
-    #     'default': {
-    #         'ENGINE': 'django.db.backends.mysql',
-    #         'NAME': 'biffduncanwebsitedb',
-    #         'USER': 'root',
-    #         'PASSWORD': 'biffduncan',
-    #     }
-    # }
+    ''' Heroku Clear MySQLDB '''
+    # Register database schemes in URLs.
+    urlparse.uses_netloc.append('mysql')
+
+    try:
+
+        # Check to make sure DATABASES is set in settings.py file.
+        # If not default to {}
+
+        if 'DATABASES' not in locals():
+            DATABASES = {}
+
+        if 'DATABASE_URL' in os.environ:
+            url = urlparse.urlparse(os.environ['DATABASE_URL'])
+
+            # Ensure default database exists.
+            DATABASES['default'] = DATABASES.get('default', {})
+
+            # Update with environment configuration.
+            DATABASES['default'].update({
+                'NAME': url.path[1:],
+                'USER': url.username,
+                'PASSWORD': url.password,
+                'HOST': url.hostname,
+                'PORT': url.port,
+            })
+
+
+            if url.scheme == 'mysql':
+                DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
+    except Exception:
+        print 'Unexpected error:', sys.exc_info()
+
+
 
 MIGRATION_MODULES = {
     
