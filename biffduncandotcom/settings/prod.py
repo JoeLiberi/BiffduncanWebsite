0d1d5fc2fd4acaecc62353c1993afc77e83e7dda
Joe Liberi <m4punk@gmail.com>
2017-01-25 23:11:40 -0500

    Updated BASE dir

--- biffduncandotcom/settings/prod.py
+++ biffduncandotcom/settings/prod.py
@@ -1,5 +1,5 @@
 from biffduncandotcom.settings.base import *
-import custom_storages
+import custom_storages, dj_database_url
 
 DEBUG=False
 
@@ -63,26 +63,26 @@ INSTALLED_APPS = [
 urllib.parse.uses_netloc.append('postgres')
 urllib.parse.uses_netloc.append('mysql')
 
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
-try:
+# try:
 
-    if 'DATABASE_URL' in os.environ:
-        url = urlparse(os.environ['DATABASE_URL'])
-        DATABASES['default'] = {
-            'NAME':     url.path[1:],
-            'USER':     url.username,
-            'PASSWORD': url.password,
-            'HOST':     url.hostname,
-            'PORT':     url.port,
-        }
-        if url.scheme == 'postgres':
-            DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
-        if url.scheme == 'mysql':
-            DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
-except:
-    print ("Unexpected error:", sys.exc_info())
+#     if 'DATABASE_URL' in os.environ:
+#         url = urlparse(os.environ['DATABASE_URL'])
+#         DATABASES['default'] = {
+#             'NAME':     url.path[1:],
+#             'USER':     url.username,
+#             'PASSWORD': url.password,
+#             'HOST':     url.hostname,
+#             'PORT':     url.port,
+#         }
+#         if url.scheme == 'postgres':
+#             DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
+#         if url.scheme == 'mysql':
+#             DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
+# except:
+#     print ("Unexpected error:", sys.exc_info())
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' 
