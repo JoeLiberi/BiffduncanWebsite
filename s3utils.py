04b14524f424394103250655629bf9ceeb30dc21
Joe Liberi <m4punk@gmail.com>
2017-01-26 12:47:57 -0500

    Added s3utils.py

--- s3utils.py
+++ s3utils.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import
 # from storages.backends.s3boto import S3BotoStorage
 
-StaticRootS3BotoStorage = lambda: storages.backends.s3boto.S3BotoStorage(location='static')
-MediaRootS3BotoStorage  = lambda: storages.backends.s3boto.S3BotoStorage(location='media')
+StaticRootS3BotoStorage = lambda: storage.backends.s3boto.S3BotoStorage(location='static')
+MediaRootS3BotoStorage  = lambda: storage.backends.s3boto.S3BotoStorage(location='media')
