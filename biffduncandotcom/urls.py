2a4183b6f4ee81110846522d1b970aaf132de694
Joe Liberi <m4punk@gmail.com>
2017-01-26 16:47:37 -0500

    Ran makemigrations on aboutus

--- biffduncandotcom/urls.py
+++ biffduncandotcom/urls.py
@@ -25,7 +25,6 @@ admin.autodiscover()
 
 urlpatterns = [
 	url(r'^admin/', include(admin.site.urls)),
-	# url(r'^tinymce/', include(tinymce.urls)),  # NOQA
 	url(r'^$', views.index, name='index'),
 	url(r'^', include('cms.urls')),
 ]
