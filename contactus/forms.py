ca8b02f191472132f427c5e600550bb903824eda
Joe Liberi <m4punk@gmail.com>
2017-02-28 11:08:35 -0500

    edited typo in forum

--- contactus/forms.py
+++ contactus/forms.py
@@ -8,7 +8,7 @@ class ContactForm(forms.Form):
     	widget=forms.TextInput(attrs={'placeholder': 'Your Email'}))
     contact_phone = forms.RegexField(regex=r'^\+?1?\d{9,15}$', 
                                 error_message = ("Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."),
-                                widget=forms.TextInput(attrs={'placeholder': 'You Phone'}))
+                                widget=forms.TextInput(attrs={'placeholder': 'Your Phone'}))
     content = forms.CharField(
         required=True,
         widget=forms.Textarea(attrs={'placeholder': 'We would love to hear from you!'})
