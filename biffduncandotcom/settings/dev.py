d18a082662cefbc1528d1db38dcab92d09e283f5
m4punk <m4punk@gmail.com>
2017-09-18 20:00:56 -0400

    customerportal

--- biffduncandotcom/settings/dev.py
+++ biffduncandotcom/settings/dev.py
@@ -6,7 +6,7 @@ ALLOWED_HOSTS = ['*']
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'biffduncandotcom', 'static'),
+    os.path.join('static'),
     # os.path.join(PROJECT_ROOT, 'static'),
 )
 STATIC_URL = '/static/'
